2d274d1b3aa260ec34ac1c9fbc40320cf50a360a
yanir <yanir.laty@gmail.com>
2024-02-18 19:57:55 +0200

    added zoom class, New Home work 2 added

---
+++ HW2/introtocsHW2/HW2a.java
@@ -0,0 +1,226 @@
+/*
+ * Be sure to submit your work in a folder called introtocsHW2
+ *
+ * Some words about submission:
+ * Code that does not compile will receive a grade of 0.
+ * Code must rigorously follow style guidelines. (15 points)
+ * Code must work without bugs exactly as described in the API and assignment instructions(50 points).
+ *
+ * We use software to detect cheating.
+ * Copying from anywhere, online or otherwise, using chat GPT or similar
+ * software, or in any way collaborating with another student is cheating and
+ * will earn the student a grade of 0.
+ *
+ * Questions regarding this assignment should be addressed in the moodle forum.
+ * Questions about this assignment sent in mail would not be responded
+ *
+ */
+package introtocsHW2;
+
+import java.util.Arrays;
+
+/**
+ * This class includes a set of functions building on strings, arrays, and 2D arrays
+ * Important note: I remind you that good coding practice should avoid code redundancy, you should have nearly all methods at 5 lines or fewer.
+ * If you need to write longer methods, you should break them up into multiple smaller methods.
+ *
+ * @author Alon Kaya
+ */
+public class HW2a {
+
+
+    /**
+     * Compute the dot product of two arrays.
+     *
+     * @param arr1 The first array.
+     * @param arr2 The second array.
+     * @return The dot product of arr1 and arr2.
+     */
+    public static double dotProduct(double[] arr1, double[] arr2) {
+        double output = 0;
+        ///////Insert your code below this line. //////////////
+        /*
+        Compute the dot product of two arrays.
+        The dot product is the sum of the products of corresponding elements.
+
+        Example:
+        arr1 = [1, 2, 3]
+        arr2 = [4, 5, 6]
+        dotProduct(arr1, arr2) should return 1*4 + 2*5 + 3*6 = 32
+
+        Note: This function should be used in other parts of the assignment- avoid code redundancy!!
+        */
+
+        /////////All student code should be above this line.////////////////
+        return output;
+    }
+
+
+    /**
+     * Additional function: Transpose a matrix.
+     *
+     * @param matrix The input matrix as a 2D array.
+     * @return The transposed matrix.
+     */
+    public static double[][] transposeMatrix(double[][] matrix) {
+        double[][] output = {{}};
+        ///////Insert your code below this line. //////////////
+        /*
+        Transpose a matrix.
+        The transpose of a matrix is obtained by swapping its rows with columns.
+
+        Example:
+        matrix = {{1, 2, 3},
+                  {4, 5, 6}}
+        transposeMatrix(matrix) should return {{1, 4}, {2, 5}, {3, 6}}
+        */
+
+        /////////All student code should be above this line.////////////////
+        return output;
+    }
+
+
+    /**
+     * Additional function: Check if a matrix is symmetric.
+     *
+     * @param matrix The input matrix as a 2D array.
+     * @return True if the matrix is symmetric, false otherwise.
+     */
+    public static boolean isMatrixSymmetric(double[][] matrix) {
+        boolean output = false;
+        ///////Insert your code below this line. //////////////
+        /*
+        Check if a matrix is symmetric.
+        A matrix is symmetric if it is equal to its transpose.
+
+        Example:
+        symmetricMatrix = {{1, 2, 3},
+                           {2, 4, 5},
+                           {3, 5, 6}}
+        isMatrixSymmetric(symmetricMatrix) should return true
+        */
+
+        /////////All student code should be above this line.////////////////
+        return output;
+    }
+
+    /**
+     * Additional function: Multiply a matrix by a scalar.
+     *
+     * @param matrix The input matrix as a 2D array.
+     * @param scalar The scalar to multiply the matrix by.
+     * @return The resulting matrix after scalar multiplication.
+     */
+    public static double[][] scalarMultiplyMatrix(double[][] matrix, int scalar) {
+        double[][] output = {{}};
+        ///////Insert your code below this line. //////////////
+        /*
+        Multiply a matrix by a scalar.
+        Each element of the matrix is multiplied by the scalar.
+
+        Example:
+        originalMatrix = {{1, 2}, {3, 4}}
+        scalarMultiplyMatrix(originalMatrix, 2) should return {{2, 4}, {6, 8}}
+        */
+
+        /////////All student code should be above this line.////////////////
+        return output;
+    }
+
+
+    /**
+     * Add a number to all values in an array.
+     *
+     * @param arr    The input array of doubles.
+     * @param number The number to be added to all values in the array.
+     * @return A new array where the number has been added to each element.
+     */
+    public static double[] addNumberToArray(double[] arr, double number) {
+        double[] result = {};
+        ///////Insert your code below this line. //////////////
+    /*
+    Add a number to all values in an array.
+
+    Example:
+    arr = {1.5, 2.5, 3.5}
+    number = 2.0
+    addNumberToArray(arr, number) should return a new array {3.5, 4.5, 5.5}
+    */
+
+        /////////All student code should be above this line.////////////////
+        return result;
+    }
+
+
+    public static void printMatrix(double[][] matrix) {
+        String output = "";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                output = output + matrix[i][j] + " ";
+            }
+        }
+        System.out.println(output);
+    }
+
+    public static boolean EqualMatrices(double[][] matrix1, double[][] matrix2) {
+        // Check if the dimensions of both matrices are the same
+        if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length) {
+            return false;
+        }
+
+        // Iterate through each element and compare
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                if (Math.abs(matrix1[i][j] - matrix2[i][j]) > 0.000001) {
+                    return false; // Found a pair of elements that are not equal
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Main method for testing the implemented functions.
+     */
+    public static void main(String[] args) {
+
+        // Test dotProduct
+        double[] dotArr1 = {1, 2, 3};
+        double[] dotArr2 = {4, 5, 6};
+        System.out.println(Math.abs(dotProduct(dotArr1, dotArr2) - 32) < 0.000001);
+
+
+        // Test transposeMatrix
+        double[][] transMatrix = {{1, 2, 3}, {4, 5, 6}};
+        double[][] resultTransMatrix = transposeMatrix(transMatrix);
+        double[][] expected1 = {{1, 4}, {2, 5}, {3, 6}};
+        System.out.println(EqualMatrices(resultTransMatrix, expected1));
+
+
+        // Test isMatrixSymmetric
+        double[][] symmetricMatrix = {{1, 2, 3}, {2, 4, 5}, {3, 5, 6}};
+        System.out.println(isMatrixSymmetric(symmetricMatrix));
+
+        // Test scalarMultiplyMatrix
+        double[][] originalMatrix = {{1, 2}, {3, 4}};
+        double[][] scalarMultipliedMatrix = scalarMultiplyMatrix(originalMatrix, 2);
+        double[][] expected2 = {{2, 4}, {6, 8}};
+        System.out.println(EqualMatrices(scalarMultipliedMatrix, expected2));
+
+
+        // Test addNumberToArray
+        double[] arrToAdd = {1.5, 2.5, 3.5};
+        double numberToAdd = 2.0;
+        double[] resultArray = addNumberToArray(arrToAdd, numberToAdd);
+        double[] expected3 = {3.5, 4.5, 5.5};
+        System.out.println(Arrays.equals(resultArray, expected3));
+
+
+    }
+}
+
+
+
+
+
+
---
+++ HW2/introtocsHW2/HW2b.java
@@ -0,0 +1,114 @@
+/*
+ * Be sure to submit your work in a folder called introtocsHW2
+ *
+ * Some words about submission:
+ * Code that does not compile will receive a grade of 0.
+ * Code must rigorously follow style guidelines. (15 points)
+ * Code must work without bugs exactly as described in the API and assignment instructions(50 points).
+ *
+ * We use software to detect cheating.
+ * Copying from anywhere, online or otherwise, using chat GPT or similar
+ * software, or in any way collaborating with another student is cheating and
+ * will earn the student a grade of 0.
+ *
+ * Questions regarding this assignment should be addressed in the moodle forum.
+ * Questions about this assignment sent in mail would not be responded
+ *
+ */
+package introtocsHW2;
+import java.util.Arrays;
+
+
+/**
+ * This class includes a set of functions building on strings, arrays, and 2D arrays
+ * Important note: I remind you that good coding practice should avoid code redundancy, you should have nearly all methods at 5 lines or fewer.
+ * If you need to write longer methods, you should break them up into multiple smaller methods.
+ *
+ * @author Alon Kaya
+ */
+
+public class HW2b {
+    //YOU SHOULD WRITE YOUR CODE BELOW THIS LINE!
+
+
+
+    //YOU SHOULD WRITE YOUR CODE ABOVE THIS LINE!
+
+    public static void printMatrix(double[][] matrix) {
+        String output = "";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                output = output + matrix[i][j] + " ";
+            }
+        }
+        System.out.println(output);
+    }
+    public static boolean EqualMatrices(double[][] matrix1, double[][] matrix2) {
+        // Check if the dimensions of both matrices are the same
+        if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length) {
+            return false;
+        }
+
+        // Iterate through each element and compare
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                if (Math.abs(matrix1[i][j] - matrix2[i][j]) > 0.000001) {
+                    return false; // Found a pair of elements that are not equal
+                }
+            }
+        }
+        return true;
+    }
+    /**
+     * Main method for testing the implemented functions.
+     */
+    public static void main(String[] args) {
+        // Test arrayToString
+        double[] arrToString = {1, 2, 3};
+//        System.out.println(arrayToString(arrToString).equals("123"));
+
+        double[][] matrix1 = {{1, 2}, {3, 4}};
+        double[][] matrix2 = {{5, 6}, {7, 8}};
+        double[][] expected = {{19,22}, {43,50}};
+//        System.out.println(EqualMatrices(multiplyMatrices(matrix1, matrix2), expected));
+
+        // Test vectorMagnitude
+        double[] vectorMag = {3, 4};
+//        System.out.println(Math.abs(vectorMagnitude(vectorMag) - 5.0) < 0.000001);
+
+        // Test isVectorOrthogonalToMatrix
+        double[] orthogonalVector = {1, 2};
+        double[] orthogonalMatrix = {2, -1};
+//        System.out.println(isVectorsOrthogonal(orthogonalVector, orthogonalMatrix));
+
+        // Test countRowsWithEvenElements
+        double[] arr = {2, 4, 6};
+//        System.out.println(checkIfEven(arr));
+
+        // Test countRowsWithEvenElements
+        double[][] evenElementMatrix = {{2, 4, 6}, {8, 10, 12}, {14, 16, 18}};
+//        System.out.println(Math.abs(countRowsWithEvenElements(evenElementMatrix) - 3) < 0.000001);
+
+
+        // Test addNumberToMatrix
+        double[][] matrixToAdd = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
+        double numberToAddToMatrix = 2.0;
+//        double[][] resultMatrixAdd = addNumberToMatrix(matrixToAdd, numberToAddToMatrix);
+        double[][] expected2 = {{3.0, 4.0, 5.0}, {6.0, 7.0, 8.0}};
+//        System.out.println(EqualMatrices(resultMatrixAdd, expected2));
+
+        String id = "54370042";
+//        System.out.println(idBikoret(id).equals("54370042-1"));
+
+        String id2 = "ABC12#777#xyD?EHz!555";
+        double[] expected3 = {8,3,6,4};
+//        System.out.println(Arrays.equals(countCharacters(id2), expected3));
+
+        char [][] charMat = {{'a','b','c','d'},{'e','f','g','h'},{'w','x','y','z'}};
+        String word = "efg";
+        double[] expected4 = {0,1,0};
+//        System.out.println(Arrays.equals(findString(charMat, word), expected4));
+    }
+}
+
+
