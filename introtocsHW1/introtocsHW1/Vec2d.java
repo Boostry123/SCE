708f1fd474039c7b2d6191dff0cce68e03b74d51
yanir <yanir.laty@gmail.com>
2024-02-19 22:08:05 +0200

    .

---
+++ introtocsHW1/introtocsHW1/Vec2d.java
@@ -0,0 +1,20 @@
+package introtocsHW1.introtocsHW1;
+
+/**
+ * An immutable 2d vector class.
+ * @author Dov Neimand
+ */
+public class Vec2d {
+    public final double x, y;
+
+    /**
+     * The constructor.
+     * @param x The x value.
+     * @param y The y value.
+     */
+    public Vec2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
