f1fa3ae575b5365076f41e545c1c9997ba0684c5
yanir <yanir.laty@gmail.com>
2024-04-07 17:38:45 +0300

    HW4

--- introtocsHW4/src/introtocsHW4/HW4.java
+++ introtocsHW4/src/introtocsHW4/HW4.java
@@ -37,6 +37,63 @@ public class HW4 {
         return -1;
     }
 
+    /**
+     * @param arr
+     * @return the number of swaps made
+     */
+    public static int insertionSort(int[] arr){
+        int counter = 0;
+        for ( int i = 1; i < arr.length; i ++){
+            int value = arr[i];
+            while (i > 0 && arr[i-1] < value){
+                arr[i] = arr[i-1];
+                counter++;
+                i = i-1;
+            }
+            arr[i] = value;
+        }
+        return counter;
+    }
+
+    /**
+     * @param arr
+     * @return the amount of object swaps made
+     */
+    public static int bubbleSort(char[] arr){
+        int counter = 0;
+        int result = bubbleSort(arr,0,0,counter);
+        return result;
+    }
+
+    /**
+     * makes arr.length **2 iterations and swaps the array to be descending
+     * @param arr
+     * @param i
+     * @param stopPoint
+     * @param counter
+     * @return the amount of swaps made
+     */
+    public static int bubbleSort(char[] arr, int i,int stopPoint,int counter){
+        if(stopPoint== Math.pow(arr.length,2)){
+            return counter;
+        }
+        if(i >= arr.length-1){
+            i =0;
+        }
+        if(arr[i] > arr[i+1]){
+            stopPoint++;
+            counter++;
+            char tmp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = tmp;
+        }else{
+            stopPoint++;
+        }
+        i++;
+        return bubbleSort(arr,i,stopPoint,counter);
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -44,6 +101,14 @@ public class HW4 {
     // testing the interpolationSearch:
         char[] arr = {'z','m','Z','W','L','K','E','E','1'};
         System.out.println(" The index is " +interpolationSearch(arr, 'E'));
+    // testing the insertionSort:
+        int[] arr2 = {24 ,10 ,15 ,25 ,13 ,7 ,19 ,4 ,21};
+        System.out.println( insertionSort(arr2));
+    // testing the bubbleSort:
+        char[] arr3 =  { 'Y', 'K', 'P', 'Z', 'N', 'H', 'T', 'E', 'V'};
+        System.out.println(bubbleSort(arr3));
     }
+
+
 
 }
