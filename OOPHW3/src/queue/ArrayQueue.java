35dfae5230f879cc15ae1afe82f3aadbd648e690
yanirla <yanir.laty@gmail.com>
2024-07-16 11:26:17 +0300

    oophw3 initial

--- OOPHW3/src/queue/ArrayQueue.java
+++ OOPHW3/src/queue/ArrayQueue.java
@@ -1,10 +1,43 @@
 package queue;
-
+//Author : Yanir Latyshev
+//Assignment : 3
 public class ArrayQueue implements Queue
 {
 
     private Object[] queue;
     private int size;
+
+    public boolean enqueue(Object element){
+        return true;
+    }
+    public Object dequeue(){
+        if (queue[0] != null){
+            Object temp = queue[0];
+            for(int i= 0; i< queue.length; i++){
+                queue[i] = queue[i+1];
+            }
+            return temp;
+        }else{
+            return null;
+        }
+    }
+    public Object peek(){
+        if (queue[0] != null){
+            return queue[0];
+        }else{
+            return null;
+        }
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean isFull(){
+        return size == 20;
+    }
+
 
 
 }
