e3fabb9e714a26e3e321ca3d8e2c528f2ece3758
yanir <yanir.laty@gmail.com>
2024-07-28 19:44:57 +0300

    only power set is left to code.

--- OOPHW3/src/Main.java
+++ OOPHW3/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         //initializing first array and adding elements to it.
         SimpleSet<Double> testArray = new SimpleSet<>(5);
         System.out.println(testArray.isEmpty());
+
         testArray.add(1.0);
         testArray.add(2.0);
         testArray.add(3.0);
@@ -44,33 +45,43 @@ public class Main {
         }
 
         // this loop uses 2 arrays to make a union between them and returns an array back.
-        /**
+
         Double[] resultUnion = SetUtils.union(test1, test2);
+        System.out.println("##### Union #####");
         for(int i = 0 ; i < resultUnion.length ; i++){
            System.out.println(resultUnion[i]);
         }
-         **/
+        System.out.println();
+
 
         //this loop uses 2 arrays to make an intersection between them and returns an array.
-        /**
+
         Double[] resultIntersection = SetUtils.intersection(test1, test2);
+        System.out.println("##### Intersection #####");
         for(int i = 0 ; i < resultIntersection.length ; i++){
             System.out.println(resultIntersection[i]);
         }
-         **/
+        System.out.println();
+
 
         //this loop uses 2 arrays to make a difference between the test1 and test2 and return an array back.
-        /**
+
         Double[] resultDifference = SetUtils.difference(test1, test2);
+        System.out.println("##### Difference #####");
         for(int i = 0; i < resultDifference.length; i++){
             System.out.println(resultDifference[i]);
         }
-         **/
+        System.out.println();
+
         //this loop uses 2 arrays to make a Symetric difference between them and returns an array back.
+
         Double[] resultSymetricDifference = SetUtils.symetricDifference(test1,test2);
+        System.out.println("##### Symetric Difference #####");
         for(int i = 0; i < resultSymetricDifference.length; i++){
             System.out.println(resultSymetricDifference[i]);
         }
+        System.out.println();
+
         Double[] powerlist = {};
 //        System.out.println(SetUtils.powerSet(powerlist));
     }
